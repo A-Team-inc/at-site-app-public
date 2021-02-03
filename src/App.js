ceda5dbc9c5f3af7c85ab50d2c0f5f9f7de02151
aapk <aapk.upwork@gmail.com>
2021-02-03 22:51:56 +0200

    first redesign / needs to change mobile version / middle section

--- src/App.js
+++ src/App.js
@@ -14,18 +14,42 @@ function App() {
   }, []);
 
 
-  
   return (
     <div className="A-Team">
       { loaderState &&  <Loader /> }
       <div className="content">
+
         <div className="hero-section">
           <div className="name">A-Team</div>
           <img src={HeroImage} className="hero-logo" alt="logo" />
+          <h2 className="greeting-text" >We are a small but super talented company with the high level it specialists.</h2>
         </div>
-        <div className="middle-section">
-          <p class="line-1 anim-typewriter">We can everything you want and do it good</p>
-          <h2>We are a small but super talented company with the high level it specialists.</h2>
+
+        {/* <div className="greeting-section book-width"> */}
+          {/* <p className="line-1 anim-typewriter">We can everything you want and do it good</p> */}
+          {/* <h2 className="greeting-text" >We are a small but super talented company with the high level it specialists.</h2> */}
+        {/* </div> */}
+
+        <div className="who-we-are-section">
+          <div className="who-we-are-content">
+            <h1 className="who-we-are">What is A-Team?</h1>
+            <div className="answer1">it's a dozens succesfull projects</div>
+            <div className="answer2">it's a skilled specialists</div>
+            <div className="answer3">it's a friendly team</div>
+          </div>
+        </div>
+        
+        <div className="footer">
+          <div className="contacts book-width">
+            <div className="artur">
+              <div className="co-founder">Co-founder Artur</div>
+              <div className="co-founder-email">artur.koval@ateam-inc.com</div>
+            </div>
+            <div className="anton"> 
+              <div className="co-founder">Co-founder Anton</div>
+              <div className="co-founder-email">anton.polomkin@ateam-inc.com</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
