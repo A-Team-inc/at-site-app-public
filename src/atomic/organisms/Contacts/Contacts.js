196722ffd6f6bc20e41ff066703358c709d8aa81
aapk <aapk.upwork@gmail.com>
2021-03-18 13:08:39 +0200

    Add contacts

--- src/atomic/organisms/Contacts/Contacts.js
+++ src/atomic/organisms/Contacts/Contacts.js
@@ -19,12 +19,15 @@ const Contacts = () => {
       </Title>
       <div className="contacts_content">
         <Text textClassName="contacts_info-title">Phone:</Text>
-        <Anchor Url="tel:+380661111111" ClassName="contacts_info">
-          +38(066)111-11-11
+        <Anchor Url="tel:+380636527771" ClassName="contacts_info">
+          +38(063)652-77-71
+        </Anchor>
+        <Anchor Url="tel:+380963248636" ClassName="contacts_info">
+          +38(096)324-86-36
         </Anchor>
         <Text textClassName="contacts_info-title">E-mail:</Text>
         <Anchor Url="mailto:test@tect.com" ClassName="contacts_info">
-          test@tect.com
+          consulting@ateam-inc.com
         </Anchor>
         <Text textClassName="contacts_info-title">Messengers:</Text>
         <div>
